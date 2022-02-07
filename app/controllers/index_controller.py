ef31b273f5cf252482004c3fd15bde03b6094832
Anand Patel <anp8@njit.edu>
2022-02-07 12:27:47 -0500

    Updated Name

--- app/controllers/index_controller.py
+++ app/controllers/index_controller.py
@@ -6,5 +6,5 @@ from  . controller import ControllerBase
 class IndexController(ControllerBase):
     @staticmethod
     def get():
-        name = "kwilliam"
+        name = "Anand Patel"
         return render_template('index.html', name=name)
